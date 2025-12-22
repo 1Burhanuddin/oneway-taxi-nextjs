11e7067147e36f8b623359a211375e0d68d11522
burhanuddin <111burhanuddin@gmail.com>
2025-12-22 11:30:45 +0530

    broken/not found links fixed

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/sample-page',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
